06b6b61f887ce867798ccda11636d13b3ccc54a8
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-04-28 12:36:16 +0300

    css and buttons

--- main.js
+++ main.js
@@ -2,93 +2,96 @@ function getComputerChoice(){
     let pcList = ["rock", "paper", "scissors"];
     let randomIndex = Math.floor(Math.random() * pcList.length);
     let pcChoice = pcList[randomIndex]
-    //console.log("pc choice: " + pcChoice);
     return pcChoice;
-
 }
-//getComputerChoice();
 
-function getHumanChoice(){
-    let a = prompt("rock? paper? scissors?");
-    let humanChoice = a.toLowerCase();
-    if(humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors"){
-        //console.log("human choice: " + humanChoice);
-        return humanChoice;
+const humanContent = document.querySelector(".humanChoice");
+const computerContent = document.querySelector(".computerChoice");
+let humanChoice = "";
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", buttonClicked);
+});
+
+function buttonClicked(){
+    if(humanScore >= 5 || computerScore >= 5) {
+        buttonsOff();
     }
-    getComputerChoice();
-    console.log("Please write only 'rock' or 'paper' or 'scissors':");
-    
-    getHumanChoice();
+    humanChoice = this.id;
+    humanContent.textContent = humanChoice;
+    computerContent.textContent = getComputerChoice();
+    let computer = computerContent.textContent;
+    let human = humanContent.textContent;
+    playRound(computer, human);
 }
-//getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(computerChoice, humanChoice){
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice();
-    //console.log(humanChoice);
-    //console.log(computerChoice);
-    if(computerChoice !== humanChoice){
-        if(humanChoice === "rock"){
-            switch(computerChoice){
-                case "scissors":
-                    console.log("%cYou win! rock beats scissors.", "color:green; font-weight:bold;");
-                    humanScore++;
-                    //console.log("human score: " + humanScore);
-                    break;
-                case "paper":
-                    console.log("%cYou lost! paper beats rock.", "color:red; font-weight:bold;");
-                    computerScore++;
-                    //console.log("pc score: " + computerScore);
-                    break;
-            }
-            console.log("pc score:" + computerScore + " - human score: " + humanScore);
-        }
-        if(humanChoice === "paper"){
-            switch(computerChoice){
-                case "scissors":
-                    console.log("%cYou lost! scissors beats paper.", "color:red; font-weight:bold;");
-                    computerScore++;
-                    //console.log("pc score: " + computerScore);
-                    break;
-                case "rock":
-                    console.log("%cYou win! paper beats rock.", "color:green; font-weight:bold;");
-                    humanScore++;
-                    //console.log("human score: " + humanScore);
-                    break;
-            }
-            console.log("pc score:" + computerScore + " - human score: " + humanScore);
-        }
-        if(humanChoice === "scissors"){
-            switch(computerChoice){
-                case "rock":
-                    console.log("%cYou lost! rock beats scissors.", "color:red; font-weight:bold;");
-                    computerScore++;
-                    //console.log("pc score: " + computerScore);
-                    break;
-                case "paper":
-                    console.log("%cYou win! scissors beats paper.", "color:green; font-weight:bold;");
-                    humanScore++;
-                    //console.log("human score: " + humanScore);
-                    break;
-            }
-            console.log(`Pc score: ${computerScore} - human score: ${humanScore}`);
-        }
-    } else {
-        console.log("%cYou are same! Try again...", "color:yellow; font-weight:bold;")
-        i--;
-    }
-
+const buttonsOff = function(){
+    buttons.forEach(button => {
+        button.disabled = true;
+    });
 }
 
-let i = 1;
-function playGame(){
-    for(; i<=5; i++){
-        playRound();
-        //console.log(i + ". round ended: ");
+function playRound(computerChoice, humanChoice){
+    if(humanScore >= 5){
+        console.log("Human won");
+        buttonsOff();
+    } 
+    if (computerScore >= 5){
+        console.log("Computer won");
+        buttonsOff();
+    }
+    if(!(humanScore >= 5 || computerScore >= 5)) {
+        if(computerChoice !== humanChoice){
+            if(humanChoice === "rock"){
+                switch(computerChoice){
+                    case "scissors":
+                        console.log("%cYou win! rock beats scissors.", "color:green; font-weight:bold;");
+                        humanScore++;
+                        break;
+                    case "paper":
+                        console.log("%cYou lost! paper beats rock.", "color:red; font-weight:bold;");
+                        computerScore++;
+                        break;
+                }
+                console.log("pc score:" + computerScore + " - human score: " + humanScore);
+            }
+            if(humanChoice === "paper"){
+                switch(computerChoice){
+                    case "scissors":
+                        console.log("%cYou lost! scissors beats paper.", "color:red; font-weight:bold;");
+                        computerScore++;
+                        break;
+                    case "rock":
+                        console.log("%cYou win! paper beats rock.", "color:green; font-weight:bold;");
+                        humanScore++;
+                        break;
+                }
+                console.log("pc score:" + computerScore + " - human score: " + humanScore);
+            }
+            if(humanChoice === "scissors"){
+                switch(computerChoice){
+                    case "rock":
+                        console.log("%cYou lost! rock beats scissors.", "color:red; font-weight:bold;");
+                        computerScore++;
+                        break;
+                    case "paper":
+                        console.log("%cYou win! scissors beats paper.", "color:green; font-weight:bold;");
+                        humanScore++;
+                        break;
+                }
+                console.log(`Pc score: ${computerScore} - human score: ${humanScore}`);
+            }
+        } else {
+            console.log("%cYou are same! Try again...", "color:yellow; font-weight:bold;")
+        }
         
     }
 }
-playGame();
+
+
+
+
