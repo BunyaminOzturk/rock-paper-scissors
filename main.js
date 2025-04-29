cbda2270a0bc4b0ec9ff4a52d3ce431e22ecb895
Bunyamin Ozturk <bunyamin.ozturk.tr@gmail.com>
2025-04-29 20:28:20 +0300

    new desing

--- main.js
+++ main.js
@@ -10,20 +10,42 @@ const computerContent = document.querySelector(".computerChoice");
 let humanChoice = "";
 const buttons = document.querySelectorAll("button");
 
+const humanScoreContent = document.querySelector("#humanScore");
+const computerScoreContent = document.querySelector("#computerScore");
+
+const winContent = document.querySelector("#win");
+
+function currentScore() {
+    humanScoreContent.textContent = humanScore;
+    computerScoreContent.textContent = computerScore;
+}
+
 buttons.forEach((button) => {
     button.addEventListener("click", buttonClicked);
 });
 
-function buttonClicked(){
-    if(humanScore >= 5 || computerScore >= 5) {
+function buttonCheck(){
+    if(humanScore >= 5){
+        console.log("Human won");
+        winContent.textContent = "YOU WON!";
+        buttonsOff();
+    } 
+    if (computerScore >= 5){
+        console.log("Computer won");
+        winContent.textContent = "COMPUTER WON!";
         buttonsOff();
     }
+}
+
+function buttonClicked(){
+    buttonCheck();
     humanChoice = this.id;
     humanContent.textContent = humanChoice;
     computerContent.textContent = getComputerChoice();
     let computer = computerContent.textContent;
     let human = humanContent.textContent;
     playRound(computer, human);
+    buttonCheck();
 }
 
 let humanScore = 0;
@@ -38,10 +60,12 @@ const buttonsOff = function(){
 function playRound(computerChoice, humanChoice){
     if(humanScore >= 5){
         console.log("Human won");
+        winContent.textContent = "YOU WON!";
         buttonsOff();
     } 
     if (computerScore >= 5){
         console.log("Computer won");
+        winContent.textContent = "COMPUTER WON!";
         buttonsOff();
     }
     if(!(humanScore >= 5 || computerScore >= 5)) {
@@ -51,10 +75,12 @@ function playRound(computerChoice, humanChoice){
                     case "scissors":
                         console.log("%cYou win! rock beats scissors.", "color:green; font-weight:bold;");
                         humanScore++;
+                        currentScore();
                         break;
                     case "paper":
                         console.log("%cYou lost! paper beats rock.", "color:red; font-weight:bold;");
                         computerScore++;
+                        currentScore();
                         break;
                 }
                 console.log("pc score:" + computerScore + " - human score: " + humanScore);
@@ -64,10 +90,12 @@ function playRound(computerChoice, humanChoice){
                     case "scissors":
                         console.log("%cYou lost! scissors beats paper.", "color:red; font-weight:bold;");
                         computerScore++;
+                        currentScore();
                         break;
                     case "rock":
                         console.log("%cYou win! paper beats rock.", "color:green; font-weight:bold;");
                         humanScore++;
+                        currentScore();
                         break;
                 }
                 console.log("pc score:" + computerScore + " - human score: " + humanScore);
@@ -77,21 +105,19 @@ function playRound(computerChoice, humanChoice){
                     case "rock":
                         console.log("%cYou lost! rock beats scissors.", "color:red; font-weight:bold;");
                         computerScore++;
+                        currentScore();
                         break;
                     case "paper":
                         console.log("%cYou win! scissors beats paper.", "color:green; font-weight:bold;");
                         humanScore++;
+                        currentScore();
                         break;
                 }
                 console.log(`Pc score: ${computerScore} - human score: ${humanScore}`);
             }
         } else {
             console.log("%cYou are same! Try again...", "color:yellow; font-weight:bold;")
+            currentScore();
         }
-        
     }
 }
-
-
-
-
